eeafbfd0ab1b1103e0551171be62c6039d022cee
4may <naoki.hanzawa.pub@gmail.com>
2020-05-12 14:54:30 +0900

    functional component

--- practice1/src/index.js
+++ practice1/src/index.js
@@ -1,11 +1,21 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
+//functional component
+//render関数の引数にHTMLコードをそのまま書くとわかりづらい。
+//以下のようにコンポーネント化(関数化)しておこう。
+function MyApp(){
+    return (
+        <ul>
+            <li>list1</li>
+            <li>list2</li>
+            <li>list3</li>
+        </ul>
+    )
+}
+
 ReactDOM.render(
-    <ul>
-        <li>list1</li>
-        <li>list2</li>
-        <li>list3</li>
-    </ul>, 
+    //MyApp自体は指定できないため、HTMLタグとして渡す。
+    <MyApp />, 
     document.getElementById("root")
 )
