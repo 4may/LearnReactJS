12065f1cbb5b4bcc99e61076c047ad79699a5d92
4may <naoki.hanzawa.pub@gmail.com>
2020-05-12 17:52:00 +0900

    practice6: cssを読み込み

---
+++ practice6/src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Header() {
+    return (
+        //classNameはJSX流の書き方。HTMLではclass=""とすることに注意。
+        <header className="navbar">世界最強を目指す</header>
+    )
+}
+
+export default Header
---
+++ practice6/src/components/MainContent.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function MainContent() {
+    return (
+        <main>This is the MainContent.</main>
+    )
+}
+
+export default MainContent
