c8e6fafc48166565811110848648f18035d48a1b
4may <naoki.hanzawa.pub@gmail.com>
2020-05-12 14:34:15 +0900

    practice1: 非順序リストの表示

---
+++ practice1/src/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+ReactDOM.render(
+    <ul>
+        <li>list1</li>
+        <li>list2</li>
+        <li>list3</li>
+    </ul>, 
+    document.getElementById("root")
+)
