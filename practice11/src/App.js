10b503695bda35999d31375f9368c936e8698367
4may <naoki.hanzawa.pub@gmail.com>
2020-05-13 14:20:33 +0900

    map example

--- practice11/src/App.js
+++ practice11/src/App.js
@@ -3,6 +3,13 @@ import React from "react"
 import Joke from "./Joke"
 
 function App() {
+    const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    const doubled = nums.map(function(num){
+        return num ** 2
+    })
+
+    console.log(doubled)
+
     return(
         <div>
             <Joke question="q1" punchLine="p1"/>
