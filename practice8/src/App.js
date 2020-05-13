16188269ec7c2dcb6909fbadbacc7de88d66bc63
4may <naoki.hanzawa.pub@gmail.com>
2020-05-13 11:30:04 +0900

    practice8: todolist

--- practice8/src/App.js
+++ practice8/src/App.js
@@ -4,7 +4,7 @@ import TodoItem from "./TodoItem"
 
 function App() {
     return (
-        <div>
+        <div className='todo-list'>
             <TodoItem />
             <TodoItem />
             <TodoItem />
--- practice8/src/TodoItem.js
+++ practice8/src/TodoItem.js
@@ -1,20 +1,9 @@
 import React from "react"
 
 function TodoItem() {
-    //部品の作成者は配置を決める。デザインは決めない。
-    const styles = {
-        width: "100px",
-        height: "30px",
-        margin: "5px", /*外側の余白*/
-        lineHeight: "30px", /*行の高さ*/
-        paddingLeft: "5px", /*ラベル内左側の余白*/
-        paddingRight: "5px", /*ラベル内右側の余白*/
-    }
-
     return (
-        <div>
-            <input type="checkbox" id='box'/>
-            <label for="box" className="label" style={styles}>check</label>
+        <div className="todo-item">
+            <input type="checkbox"/>
             <p>Placeholder text here</p>
         </div>
     )
