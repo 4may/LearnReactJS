d7d34ce9f95d361dfdb34f81ff33039b8c83ddfe
4may <naoki.hanzawa.pub@gmail.com>
2020-05-13 12:26:59 +0900

    practice10: 修正前。冗長なコンポーネントを一つにまとめる。

---
+++ practice10/src/App.js
@@ -0,0 +1,37 @@
+import React from "react"
+
+function App() {
+    return(
+        <div className="contacts">
+            <div className="contact-card">
+                <img src="http://placekitten.com/300/200"/>
+                <h3>Mr. Whiskerson</h3>
+                <p>Phone: (212) 555-1234</p>
+                <p>Email: mr.whiskaz@catnap.meow</p>
+            </div>
+
+            <div className="contact-card">
+                <img src="http://placekitten.com/400/200"/>
+                <h3>Fluffykins</h3>
+                <p>Phone: (212) 555-2345</p>
+                <p>Email: fluff@me.com</p>
+            </div>
+
+            <div className="contact-card">
+                <img src="http://placekitten.com/400/300"/>
+                <h3>Destroyer</h3>
+                <p>Phone: (212) 555-3456</p>
+                <p>Email: ofwordls@yahoo.com</p>
+            </div>
+
+            <div className="contact-card">
+                <img src="http://placekitten.com/200/100"/>
+                <h3>Felix</h3>
+                <p>Phone: (212) 555-4567</p>
+                <p>Email: thecat@hotmail.com</p>
+            </div>
+        </div>
+    )
+}
+
+export default App
