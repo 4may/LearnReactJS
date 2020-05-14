c0e9bc09632fde1c3b79b9cda35f4dc24e5e78b9
4may <naoki.hanzawa.pub@gmail.com>
2020-05-14 16:17:50 +0900

    practice17: react state(my answer)

---
+++ practice17/src/App.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+class App extends React.Component{
+    constructor(){
+        super()
+        this.state = {
+            name : "Simon",
+            age : 17
+        }
+    }
+
+    render(){
+        return(
+            <div>
+                <h1>{this.state.name}</h1>
+                <h3>{this.state.age} years old</h3>
+            </div>
+        )
+    }
+}
+
+export default App
