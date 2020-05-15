1668805b88f0b0a7334e93e3b62d6ab6334f6ba4
4may <naoki.hanzawa.pub@gmail.com>
2020-05-15 14:04:10 +0900

    practice24: ライフサイクルメソッドについて加筆

--- practice24/src/App.js
+++ practice24/src/App.js
@@ -13,19 +13,51 @@ class App extends Component{
     reactでは、複数のcomponent間でpropやstateのやり取りが行われる。
     やり取りの際には、複数のメソッドが呼ばれる。その仲介役となるメソッドのことをlifecycle methodと呼ぶ。
     ここでは、代表的なlifecycleメソッドを学んでみよう。
+
+    公式のチートシートがある。わかりやすい。
+    https://projects.wojtekmaj.pl/react-lifecycle-methods-diagram/
+
+    3つの非推奨メソッドについては、公式のブログポストを読む
+    https://ja.reactjs.org/blog/2018/03/29/react-v-16-3.html#component-lifecycle-changes
+    https://ja.reactjs.org/blog/2018/03/27/update-on-async-rendering.html
+    要点は以下の通り。
+    * 3つのメソッドがエラーハンドリングの妨げとなり、結果としてメモリーリークを引き起こしていた。
+    * 3つのメソッドを廃止し、新しいメソッドを追加することで、非同期的なrenderingがよりわかりやすくできるようになる。
+        * 3つのメソッドは誤用を招いていた
+    * メジャーバージョン17までは使うことができる。(既存APIの移行期間を設けている。)
     */
+
+    static getDrivedStateFromProps(props, state){
+        //propsからstateを更新するメソッド
+        //UNSAFE_componentWillReceiveProps()のsafe版
+    }
+
+    getSnapshotBeforeUpdate(){
+        // 更新前に現在のバックアップを作成するメソッド
+    }
+
+    /*!! 非推奨 !!*/
+    UNSAFE_componentWillMount(){
+
+    }
+
     componentDidMount(){
         //初めてrenderした後に呼ばれるメソッド。
         //主に、外部リソースからデータをロードする時に使われる。
         //constructor()や、render()ではデータをロードしないこと。
     }
 
-    /*!! 非推奨になった !!*/
+    /*!! 非推奨 !!*/
     UNSAFE_componentWillReceiveProps(nextProps){
         //親コンポーネントからpropsを受け取る時に呼ばれるメソッド
         if(nextProps.whatever !== this.props.whatever){
             //do something important here
         }
+    }
+
+    /*!! 非推奨 !!*/
+    UNSAFE_componentWillUpdate(){
+
     }
 
     /*componentWillReceiveProps の代わりとなる新しいメソッd*/
