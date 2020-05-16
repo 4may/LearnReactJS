3fcf3bc8f87385c8481ae8e74fae8ab18f9fde9f
4may <naoki.hanzawa.pub@gmail.com>
2020-05-16 12:17:30 +0900

    text, textarea, checkbox, radio, selectについて学んだ。

--- practice31/src/App.js
+++ practice31/src/App.js
@@ -1,24 +1,29 @@
 import React, {Component} from 'react'
 
+//formikが便利
+
 class App extends Component{
     constructor(){
         super()
 
         this.state = {
             firstName : "",
-            lastName : ""
+            lastName : "",
+            comment : "",
+            isFriendly : true,
+            gender : "",
+            favColor : "blue"
         }
 
         this.handleChange = this.handleChange.bind(this)
     }
 
     handleChange(event){
-        const {name, value} = event.target
+        //checkboxの場合、valueプロパティの代わりにcheckedプロパティを使う必要がある。
+        //そのため、typeでcheckboxとcheckbox以外とを棲み分けるひつようがある。
+        const { name, value, type, checked } = event.target
 
-        this.setState(
-            //[name]によって、name変数の値を取得する
-            { [name] : value }
-        )
+        type === "checkbox" ? this.setState({ [name] : checked }) : this.setState({ [name] : value })
     }
 
     render(){
@@ -39,7 +44,58 @@ class App extends Component{
                     onChange={this.handleChange}
                     placeholder='Last Name'
                 />
+                <br />
+                <textarea 
+                    value={"some default value"}
+                    name='comment'
+                    onChange={this.handleChange}
+                />
+                <br />
+                <label>
+                    <input 
+                        type='checkbox' 
+                        checked={this.state.isFriendly}
+                        name="isFriendly"
+                        onChange={this.handleChange}
+                    /> isFriendly?
+                </label>
+                <br/>
+                <label>
+                    <input 
+                        type='radio'
+                        name="gender"
+                        value='male'
+                        checked={this.state.gender === "male"} /*チェックをつける条件を定義する */
+                        onChange={this.handleChange}
+                    /> Male
+                </label>
+                <label>
+                    <input 
+                        type='radio' 
+                        name="gender"
+                        value='female'
+                        checked={this.state.gender === "female"} /*チェックをつける条件を定義する */
+                        onChange={this.handleChange}
+                    /> Female
+                </label>
+
+                <br/>
+                <label>Favorite color:</label>
+                <select
+                    name='favColor'
+                    value={this.state.favColor}
+                    onChange={this.handleChange}
+                >
+                    <option value="blue">Blue</option>
+                    <option value="green">Green</option>
+                    <option value="red">Red</option>
+                    <option value="orange">Orange</option>
+                    <option value="yellow">Yellow</option>
+                </select>
+                
                 <h1>{this.state.firstName} {this.state.lastName}</h1>
+                <h2>You are a {this.state.gender}</h2>
+                <h2>Your favorite color is {this.state.favColor}</h2>
             </div>
         )
     }
